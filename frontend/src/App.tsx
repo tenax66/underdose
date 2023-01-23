5af883baecb7367a2fcfc4523b6088aaf9e0a984
tenax66 <115792382+tenax66@users.noreply.github.com>
2023-01-23 22:00:15 +0900

    apply antd layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { TwitterTweetEmbed } from "react-twitter-embed";
+import { Typography, Layout, Menu, theme } from "antd";
 
 type Tweet = {
   edit_history_tweet_ids: string[];
@@ -7,7 +8,14 @@ type Tweet = {
   text: string;
 };
 
+const { Header, Content, Footer } = Layout;
+const { Title } = Typography;
+
 const App: React.FC = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   const [tweets, setTweets] = React.useState<Tweet[] | null>(null);
 
   React.useEffect(() => {
@@ -25,15 +33,25 @@ const App: React.FC = () => {
   }, []);
 
   return (
-    <div className="container tweets">
-      <h1>Tweets</h1>
-
-      {tweets?.map((tweet: Tweet) => (
-        <div key={tweet.id}>
-          <TwitterTweetEmbed tweetId={tweet.id} />
+    <Layout>
+      <Header style={{ position: "sticky", top: 0, zIndex: 1, width: "100%" }}>
+        <Title style={{ color: "white", paddingTop: "5px" }}>Underdose</Title>
+        <Menu theme="dark" mode="horizontal" items={[]} />
+      </Header>
+      <Content className="site-layout" style={{ padding: "0 50px" }}>
+        <div style={{ padding: 24, minHeight: 380, background: colorBgContainer }}>
+          <div className="container tweets">
+            <h1>Tweets</h1>
+            {tweets?.map((tweet: Tweet) => (
+              <div key={tweet.id}>
+                <TwitterTweetEmbed tweetId={tweet.id} />
+              </div>
+            ))}
+          </div>
         </div>
-      ))}
-    </div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>Underdose ©2023</Footer>
+    </Layout>
   );
 };
 
